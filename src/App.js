232b6d898459eb09581db82d3794d59c8158f4e2
Jouni K. Seppänen <jks@iki.fi>
2019-05-26 15:42:56 +0300

    Siistimistä

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import fi from 'date-fns/locale/fi';
 import Lomake from './Lomake';
 
 import './css/index.css';
-import './css/skeleton.css';
 import 'react-datepicker/dist/react-datepicker.css';
 
 registerLocale('fi', fi);
