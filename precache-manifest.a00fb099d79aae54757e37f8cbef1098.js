b209afb6153fb7e4c15f67e44a7e602b4604953a
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-07-10 15:24:49 +0000

    Deploy matkalasku.matematiikkakilpailut.fi to github.com/matematiikkakilpailut/matkalasku.git:gh-pages

---
+++ precache-manifest.a00fb099d79aae54757e37f8cbef1098.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cb2d6a07190f7542dd1a6af59608f10",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5c91503f97c258a52f20",
+    "url": "/static/css/2.cc034990.chunk.css"
+  },
+  {
+    "revision": "27b5a0ced0643f87af26",
+    "url": "/static/css/main.ce8964f3.chunk.css"
+  },
+  {
+    "revision": "5c91503f97c258a52f20",
+    "url": "/static/js/2.0d41e73f.chunk.js"
+  },
+  {
+    "revision": "27b5a0ced0643f87af26",
+    "url": "/static/js/main.8c6edff3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "690eed16f325cbcfebb1932b5cf61c6b",
+    "url": "/static/media/PublicSans-Light.690eed16.woff2"
+  },
+  {
+    "revision": "ef0bab1ac632be42ecd66f27410ff144",
+    "url": "/static/media/PublicSans-Light.ef0bab1a.ttf"
+  },
+  {
+    "revision": "f164ed0024ed068b1a2882c25c8ff33f",
+    "url": "/static/media/PublicSans-Light.f164ed00.woff"
+  },
+  {
+    "revision": "342b80605a0d25e8fe414cfade33cfab",
+    "url": "/static/media/PublicSans-SemiBold.342b8060.woff"
+  },
+  {
+    "revision": "529a386678cb3cda64f032e04cb9da3e",
+    "url": "/static/media/PublicSans-SemiBold.529a3866.woff2"
+  },
+  {
+    "revision": "626e4f2afc0060e0f2f69c6b916cc476",
+    "url": "/static/media/PublicSans-SemiBold.626e4f2a.ttf"
+  },
+  {
+    "revision": "9e421250a44533eaa85b1a392c1f672c",
+    "url": "/static/media/baseline-add-24px.9e421250.svg"
+  },
+  {
+    "revision": "bf808ebb617488b7693e371fe6e2d99e",
+    "url": "/static/media/baseline-delete_forever-24px.bf808ebb.svg"
+  }
+]);
